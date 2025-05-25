0a7a752cc02b4e4cee51a153daf06a584b6cb837
Mikołaj Skałka <skalkamikolaj@gmail.com>
2025-05-25 15:01:33 +0200

    Sonar scan fixes

--- exercise4/routes/routes.go
+++ exercise4/routes/routes.go
@@ -5,24 +5,30 @@ import (
 	"github.com/mikolajskalka/ebiznes/exercise4/controllers"
 )
 
+// Route path constants
+const (
+	ProductByIDPath  = "/products/:id"
+	CategoryByIDPath = "/categories/:id"
+)
+
 // Configure all routes for the application
 func SetupRoutes(e *echo.Echo) {
 	// Product routes
 	e.GET("/products", controllers.GetAllProducts)
-	e.GET("/products/:id", controllers.GetProduct)
+	e.GET(ProductByIDPath, controllers.GetProduct)
 	e.POST("/products", controllers.CreateProduct)
-	e.PUT("/products/:id", controllers.UpdateProduct)
-	e.DELETE("/products/:id", controllers.DeleteProduct)
+	e.PUT(ProductByIDPath, controllers.UpdateProduct)
+	e.DELETE(ProductByIDPath, controllers.DeleteProduct)
 	e.GET("/products/category/:categoryId", controllers.GetProductsByCategory)
 	e.GET("/products/price-range", controllers.GetProductsByPriceRange)
 
 	// Category routes
 	e.GET("/categories", controllers.GetAllCategories)
 	e.GET("/categories/with-products", controllers.GetCategoriesWithProducts)
-	e.GET("/categories/:id", controllers.GetCategory)
+	e.GET(CategoryByIDPath, controllers.GetCategory)
 	e.POST("/categories", controllers.CreateCategory)
-	e.PUT("/categories/:id", controllers.UpdateCategory)
-	e.DELETE("/categories/:id", controllers.DeleteCategory)
+	e.PUT(CategoryByIDPath, controllers.UpdateCategory)
+	e.DELETE(CategoryByIDPath, controllers.DeleteCategory)
 	e.GET("/categories/search", controllers.SearchCategoriesByName)
 
 	// Cart routes
